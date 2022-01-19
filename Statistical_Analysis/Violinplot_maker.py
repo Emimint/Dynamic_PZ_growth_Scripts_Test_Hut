ccc8efdfad7df4c0a7bb708206c5b1cf30e71c33
Emimint <emilie.echevin@gmail.com>
2022-01-18 19:29:17 -0500

    correct violin_maker.py file

--- Statistical_Analysis/Violinplot_maker.py
+++ Statistical_Analysis/Violinplot_maker.py
@@ -25,23 +25,13 @@ filename = "/home/user/Desktop/Project_2/210809_LEAF_DATA_EE/ALL_LEAF_growth_nig
 dest_path = '/home/user/Desktop'
 
 File_Info = pd.read_csv(filename).dropna()
-# File_Info = File_Info[File_Info.Zone.isin(["boundary","APZ","primordium"])]
-# File_Info = File_Info[File_Info.BF_value.isin([-4,-3,-2])]
-# File_Info.loc[(File_Info['Zone'] == "boundary"),'Zone'] = "primordium+boundary"
-# File_Info.loc[(File_Info['Zone'] == "primordium"),'Zone'] = "primordium+boundary"
 
 File_Info = File_Info[File_Info.Zone.isin(["CZ", "APZ"])]
 
 File_Info = File_Info.sort_values('Zone')
 
 print(File_Info)
-# # print File_Info1
-# # print Final_DF
-# print (File_Info['Label'].duplicated())
-# print (File_Info['Label'].unique())
-# print (len(File_Info['Label'].unique()))
-# print (len(File_Info['Label'].duplicated()))
-# exit()
+
 kwargs = {'edgecolor': "black",  # for edge color
           'linewidth': 0.3,
           'size': 1.5  # line width of spot
@@ -53,12 +43,6 @@ ax = sns.swarmplot(x="BF_value", y="Growth", data=File_Info, hue="Zone", dodge=T
 
 fig = plt.gcf()
 ax.get_legend().remove()
-
-# fig.subplots_adjust(bottom=1)
-
-# plt.show()
-# exit()
-# out_file_name = os.path.join(dest_path,"all_samples_meristem_zones_overall_growth_violinplot_CZ_PZ.png")
 out_file_name = os.path.join(dest_path, "Growth_CV_vs_APZ_by_BF_catplot_violinplot_colored_violins.png")
 # plt.show()
 ax.figure.savefig(out_file_name, transparent=True, dpi=300)
